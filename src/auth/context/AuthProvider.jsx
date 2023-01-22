0b5de510a05324e63fcd94cd69acbd63abb35cfb
Alejandro González Reyes <alexglzrys@gmail.com>
2023-01-22 16:36:07 -0600

    mantener el usuario activo - localStorage

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,25 +3,38 @@ import { types } from "../types/types";
 import { AuthContext } from "./AuthContext";
 import { authReducer } from "./authReducer";
 
-const initialState = {
+/*const initialState = {
   logged: false,
+};*/
+
+// Función inicializadora de estado. (Se genera con base en la lectura de localstorage)
+// Cada vez que se refresque la pantalla se ejecutará esta función y recuperará el usuario de localstorage
+const init = () => {
+  // No es buena práctica acceder al Localstorage desde las acciones definidas en el reducer
+  const user = JSON.parse(localStorage.getItem("user"));
+  // Si hay usuario entonces está logeado (true)
+  return {
+    logged: !!user,
+    user: user,
+  };
 };
 
 export const AuthProvider = ({ children }) => {
   // Especificar el reducer a proveer en este contexto
-  const [authState, dispatch] = useReducer(authReducer, initialState);
+  const [authState, dispatch] = useReducer(authReducer, {}, init);
 
   // Funciones a compartir en el contexto
 
   const login = (name = "") => {
+    const user = { id: "ABCD", name };
     // definir la acción a despachar tras hacer login
     const action = {
       type: types.login,
-      payload: {
-        id: "ABC",
-        name,
-      },
+      payload: user,
     };
+    // Grabar en localStorage el nombre del usuario logeado
+    localStorage.setItem("user", JSON.stringify(user));
+    // Despachar acción (en este punto damos por hecho que el login fue correcto en el backend)
     dispatch(action);
   };
 
